49d527d277cb8dcbb40d76a59399a616879b2b68
Imre Kelényi <imre.kelenyi@gmail.com>
2022-02-23 11:10:36 +0100

    remove score

--- src/Bowling.php
+++ src/Bowling.php
@@ -4,15 +4,13 @@ namespace Bowling;
 
 final class Game
 {
-    private int $score = 0;
-
     public function __construct()
     {
 
     }
 
     public function getScore(): int {
-        return $this->score;
+        return 0;
     }
 
     public function roll(int $n)
